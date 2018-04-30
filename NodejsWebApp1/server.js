e26266550712db3e2aa3d3339a64ffd3b8182f0a
TomHuang <huangjinj@live.com>
2018-04-30 09:28:21 +0800

    File System

--- NodejsWebApp1/server.js
+++ NodejsWebApp1/server.js
@@ -1,8 +1,54 @@
+//import { unescape } from 'querystring';
+
 'use strict';
 var http = require('http');
+var url = require('url');
+var path = require('path');
+var fs = require('fs');
 var port = process.env.PORT || 1337;
 
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"
+};
+
 http.createServer(function (req, res) {
-    res.writeHead(200, { 'Content-Type': 'text/plain' });
-    res.end('Hello World\n');
+
+    var uri = url.parse(req.url).pathname;
+    var fileName = path.join(process.cwd(), unescape(uri));
+    console.log('Loading' + uri);
+    var stats;
+
+    try {
+        stats = fs.lstatSync(fileName);
+    }
+    catch(e) {
+        res.writeHead(404, { 'Content-type': 'text/plain' });
+        res.write('404 Not Found\n');
+        res.end();
+        return;
+    }
+
+    //check file or directory
+    if (stats.isFile()) {
+        var mimeType = mimeTypes[path.extname(fileName).split(".").reverse()[0]];
+        res.writeHead(200, { 'Content-type': mimeType });
+
+        var fileStream = fs.createReadStream(fileName);
+        fileStream.pipe(res);
+    }
+    else if (stats.isDirectory()) {
+        res.writeHead(302, { 'Location': 'index.html' });
+        res.end();
+    }
+    else {
+        res.writeHead(500, { 'Content-type': 'text/plain' });
+        res.write('500 Internal Error \n');
+        res.end();
+    }
+
 }).listen(port);
